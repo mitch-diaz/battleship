4d5a3d76f84cb99d11700d1ad6d11f090935d62a
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2022-08-09 19:55:05 -0400

    started coding ship class

---
+++ game-classes.js
@@ -0,0 +1,37 @@
+class Ship {
+    constructor(size) {
+        this.health = size;
+        this.damage = 0;
+        this.location = []
+    }
+
+    setLocation(clickedDivCoordinates){
+        let tempX = clickedDivCoordinates.x
+        let tempY = clickedDivCoordinates.y
+        const orianetation = "V" //prompt("V or H?: ").toUpperCase
+        let tempLocation = [];
+        // console.log("line 13");
+        tempLocation.push(clickedDivCoordinates)
+        if (orianetation === 'H') {
+            console.log(clickedDivCoordinates)
+            for (let i = 0; i < this.health - 1; i++) {
+                let tempDivCoordinates = {x: parseInt(tempX), y: parseInt(tempY) + i+1};
+                tempLocation.push(tempDivCoordinates)
+            }
+            console.log(tempLocation);
+        } else if (orianetation === 'V') {
+            console.log(clickedDivCoordinates)
+            for (let i = 0; i < this.health - 1; i++) {
+                let tempDivCoordinates = {x: parseInt(tempX) + i+1, y: parseInt(tempY)};
+                tempLocation.push(tempDivCoordinates)
+            }
+            console.log(tempLocation);
+        }
+        this.location.push(clickedDivCoordinates);
+
+    }
+
+    receiveDamage() {
+        this.damage++;
+    }
+}
