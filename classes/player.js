5b799a9f797e2528424e59139a26eeee8d4644e3
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2022-08-13 18:00:28 -0400

    work done on player class, html file script link was commented out

--- classes/player.js
+++ classes/player.js
@@ -1,11 +1,40 @@
 class Player {
-  constructor(theAmmo) {
-    this.ammo = theAmmo;
+  constructor(playerName) {
+    this.name = playerName;
+    this.shotAttempts = 0;
   }
 
-    attack() {
-        return this.health;
-    }
-
-
+  boardShot(clickShot) {
+    const missedMsg = "It's a miss.";
+    const hitMsg = "It's a hit!";
+    const alreadyHitMsg = "You hit this ship before.";
+    const sunkMsg = "You sunk my ${shipName}";
+    const allSunkMsg = "You sank all of my battleships in ${shotAttempts} attempts."
+        
+    this.gameBoard[x][y];
+            
+        let shipArray = ship.location; // find ship array locations
+        let hits = ship.damage; // where hits are recorded
+      
+          // hits.push(1);
+          console.log({clickShot});
+        
+    
+  }
 }
+
+
+
+
+
+
+// Click function to update the top box with result of click
+
+// document.querySelectorAll('.square').forEach((boardDiv) => {
+//   boardDiv.addEventListener('click', () => {
+//     console.log({boardDiv});
+
+//     const 
+//   })
+// });
+
