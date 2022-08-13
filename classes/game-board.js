28fe2577c1574a8a36f9a4e2db0cfc9e438e2174
Zequi Aguirre <zequi4real@gmail.com>
2022-08-13 10:41:10 -0400

    split classes

---
+++ classes/game-board.js
@@ -0,0 +1,42 @@
+class Gameboard {
+  constructor() {
+    
+  }
+
+    addGameBoard() {
+    let boardCode = ""
+    const gridSize = 10;
+    for (let x = 1; x <= gridSize; x++) {
+      for (let y = 1; y <= gridSize; y++) {
+        boardCode += `<div class="square" id=${x}-${y} data-position-x="${x}" data-position-y="${y}">X=${x}
+            Y=${y}</div>`;
+      }
+    }
+    return boardCode
+  }
+
+  addShip(coordinates) {
+    // get coordinates
+    // pick direction H or V
+    // count horizontally or vertically if enough space
+    // collision check
+    // if all good add ship
+  }
+
+    addPlayer() {
+
+    }
+
+    displayScoreboard() {
+        // display scoreboard info:
+        // Hits, Sunk ships, Total points
+    }
+
+    displayFiringReport() {
+        
+    }
+
+    displaySunkShips() {
+
+    }
+}
