8493bb1cd8cbf2a264814fb9eb2025db91c3ce5d
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2022-08-16 00:17:33 -0400

    imported font in css, changed class to id in html file, still working on player class

--- classes/player.js
+++ classes/player.js
@@ -4,37 +4,27 @@ class Player {
     this.shotAttempts = 0;
   }
 
-  boardShot(clickShot) {
-    const missedMsg = "It's a miss.";
-    const hitMsg = "It's a hit!";
-    const alreadyHitMsg = "You hit this ship before.";
-    const sunkMsg = "You sunk my ${shipName}";
-    const allSunkMsg = "You sank all of my battleships in ${shotAttempts} attempts."
-        
+  countBoardClicks() {
     this.gameBoard[x][y];
-            
-        let shipArray = ship.location; // find ship array locations
-        let hits = ship.damage; // where hits are recorded
-      
-          // hits.push(1);
-          console.log({clickShot});
-        
-    
+    this.shotAttempts++
   }
+
 }
 
 
+//   displayTopMsg() {
+//     let clickOnDiv = document.querySelector('.square');
+//     clickOnDiv.addEventListener('click', () => {
+//       let topMsg = document.querySelector('#clickResultMsg, span');
+//       topMsg.innerText = "It's a miss.";
+//       console.log(clickOnDiv);
+//     })
+//   }
+  
+// }
 
-
-
-
-// Click function to update the top box with result of click
-
-// document.querySelectorAll('.square').forEach((boardDiv) => {
-//   boardDiv.addEventListener('click', () => {
-//     console.log({boardDiv});
-
-//     const 
-//   })
-// });
-
+// const missedMsg = "It's a miss.";
+// const hitMsg = "It's a hit!";
+// const alreadyHitMsg = "You hit this ship before.";
+// const sunkMsg = "You sunk my ${shipName}";
+// const allSunkMsg = "You sank all of my battleships in ${shotAttempts} attempts."
