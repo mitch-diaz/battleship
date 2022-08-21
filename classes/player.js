9dd51680386910a6d619cdca3ece35acc2b94987
Zequi Aguirre <zequi4real@gmail.com>
2022-08-20 21:11:06 -0400

    player one, then player two and game over (could be used for unlimited players)

--- classes/player.js
+++ classes/player.js
@@ -5,7 +5,7 @@ class Player {
     this.shipsToHit = 22;
     this.attempts = 0;
     this.board = [];
-    this.gameBoard = ""
+    this.gameBoard = "";
   }
 
   checkIfhit(coordinates) {
@@ -26,15 +26,35 @@ class Player {
       if (this.shipsToHit === 0) {
         let theMsg = document.querySelector("#messageArea, span");
         theMsg.innerText = `You sank all my battleships in ${this.attempts} attempts.`;
-        // setTimeout(function () {
-        //   alert("game over");
-        // }, 500);
-        let player2 = this.gameBoard.players[1]
 
-        console.log(player2);
+        this.gameBoard.players.forEach((player) => {
+            console.log(this.name);
 
-        player2.displayBoard();
-    document.getElementById("currentPlayer").innerText = player2.name;
+            let currentPlayerIndex;
+            for (let i = 0; i < this.gameBoard.players.length; i++){
+                if (this.gameBoard.players[i].name === this.name) {
+                    currentPlayerIndex = i
+                }
+            }
+
+            console.log(currentPlayerIndex);
+            let lastPlayerIndex = this.gameBoard.players.length - 1
+            console.log(this.gameBoard.players[lastPlayerIndex].name);
+          if (this.gameBoard.players[lastPlayerIndex].name !== this.name) {
+            console.log(this.gameBoard.players[currentPlayerIndex + 1]);
+            this.gameBoard.players[currentPlayerIndex + 1].displayBoard();
+            document.getElementById("currentPlayer").innerText = this.gameBoard.players[1].name;
+          } else if (this.gameBoard.players[lastPlayerIndex].name === this.name){
+
+              setTimeout(function () {
+                alert("game over");
+              }, 500);
+          }
+          
+        });
+
+        
+        
       }
     } else {
       coordinates.classList.add("miss");
@@ -46,17 +66,10 @@ class Player {
 
   displayBoard() {
     // console.log(this.board);
-    let gameBoardView = document.getElementById('game-board');
-    gameBoardView.innerHTML = ""
+    let gameBoardView = document.getElementById("game-board");
+    gameBoardView.innerHTML = "";
     this.board.forEach((div) => {
-
-        gameBoardView.appendChild(div)
-    })
-    
-    }
-    
-  
-
-  
-
+      gameBoardView.appendChild(div);
+    });
+  }
 }
